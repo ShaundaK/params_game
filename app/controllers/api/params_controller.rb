3c6ea0326cde991426fe833cc1d94a527ff1247d
ShaundaK <sdking1913@gmail.com>
2018-10-14 10:30:19 -0500

    name message works

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,5 +1,11 @@
 class Api::ParamsController < ApplicationController
   def game
+    @names = params[:name].upcase
+    #if the name begins with A, add a message
+    # if names[0] == "A"
+    if @names.start_with? ("A")
+      @names = "Your name begins with A"
+    end
     render "params_game.json.jbuilder"
   end
 end
